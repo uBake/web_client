4de933977974e82a587aa61dc6f718562655c9fa
holakirr <kpetunin@proton.me>
2022-11-24 19:31:50 -0600

    Adds ToolbarTitle story

---
+++ src/stories/Components/toolbar/ToolbarTitle.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta } from '@storybook/react';
+
+import { ToolbarTitle as StoryBase } from '../../../components/Toolbar/ToolbarTitle/ToolbarTitle';
+
+export default {
+  title: 'Components',
+  component: StoryBase,
+  argTypes: {}
+} as ComponentMeta<typeof StoryBase>;
+
+export const ToolbarTitle = {
+  args: {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    image: {
+      src: 'https://picsum.photos/200',
+      unoptimized: true
+    }
+  }
+};
