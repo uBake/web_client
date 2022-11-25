29530a5983739f1bd81b70b9055fbf0e905996c3
holakirr <kpetunin@proton.me>
2022-11-24 20:52:33 -0600

    Adds helper for Toolbar

--- src/stories/helpers/index.ts
+++ src/stories/helpers/index.ts
@@ -1,7 +1,14 @@
+import { IconButtonProps } from './../../components/buttons/Button/Button';
+
 export const genFakeTab = (id: string) => ({
   id: id.toString(),
   title: 'Tab ' + id,
   onTabSelect: (id: string) => {
     console.log('Selected id is ', id);
   }
+});
+
+export const genFakeIconButton = (id: string): IconButtonProps => ({
+  id,
+  icon: 'Placeholder'
 });
